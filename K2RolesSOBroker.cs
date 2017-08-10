6b7ac87c3a81b61701aab2f84a203b153a5210de
Prajwal Doddaguni Shambhu <prajwal@k2.com>
2017-08-10 14:27:03 +0400

    Add files via upload

---
+++ K2RolesSOBroker.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SourceCode.SmartObjects.Services.ServiceSDK;
+using SourceCode.SmartObjects.Services.ServiceSDK.Objects;
+using SourceCode.SmartObjects.Services.ServiceSDK.Types;
+using System.Data;
+
+namespace K2ME.Field.Roles
+{
+    class K2RolesSOBroker : ServiceAssemblyBase
+    {
+        #region private data members
+
+        private string _K2ServerName;
+        private int _PortNo;
+        SourceCode.SmartObjects.Client.SmartObjectClientServer cServer;
+
+        #endregion 
+
+        public override string GetConfigSection()
+        {
+            base.Service.ServiceConfiguration.Add("K2ServerName", true, 5555);
+            base.Service.ServiceConfiguration.Add("Port", true, "DLX");
+            return base.GetConfigSection();
+        }
+        public override string DescribeSchema()
+        {
+            base.Service.Name = "K2RolesSMO";
+            base.Service.MetaData.DisplayName = "K2Roles as SMO";
+            base.Service.MetaData.Description = "";
+
+            K2RolesSOProperties wrkProperties = new K2RolesSOProperties();
+            base.Service.ServiceObjects.Add(wrkProperties.DescribeServiceObjects());
+
+            return base.DescribeSchema();
+        }
+        public override void Extend()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        public override void Execute()
+        {
+            ValidateConfigSection();
+            base.ServicePackage.ResultTable = null;
+            DataTable result = new DataTable("Result");
+            try
+            {
+
+                foreach (ServiceObject serviceObj in base.Service.ServiceObjects)
+                {
+                    foreach (Method method in serviceObj.Methods)
+                    {
+                        Dictionary<string, object> properties = new Dictionary<string, object>();
+                        foreach (Property property in serviceObj.Properties)
+                        {
+                            if ((property.Value != null) && (!string.IsNullOrEmpty(property.Value.ToString())))
+                            {
+                                properties.Add(property.Name, property.Value);
+                            }
+                        }
+
+                        // build the method parameters collection
+                        Dictionary<string, object> parameters = new Dictionary<string, object>();
+                        foreach (MethodParameter parameter in method.MethodParameters)
+                        {
+                            if ((parameter.Value != null) && (!string.IsNullOrEmpty(parameter.Value.ToString())))
+                            {
+                                parameters.Add(parameter.Name, parameter.Value);
+                            }
+                        }
+
+                        if (serviceObj.Name.ToString() == "K2FieldRoles")
+                        {
+                            if (method.Name == "IsInRole")
+                            {
+                                result = IsinRole(properties, parameters);
+                            }
+                            if (method.Name == "GetRoleUsersEmails")
+                            {
+                                result = RoleUsersEmails(properties, parameters);
+                            }
+                            if (method.Name == "GetRoleUsersEmailsonIndex")
+                            {
+                                result = RoleUsersEmailswithIndex(properties, parameters);
+                            }
+                        }
+                    }
+                    base.ServicePackage.ResultTable = result;
+
+                    base.ServicePackage.IsSuccessful = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                base.ServicePackage.IsSuccessful = false;
+                base.ServicePackage.ServiceMessages.Add(new ServiceMessage(ex.Message, MessageSeverity.Error));
+            }
+
+        }
+
+        private DataTable RoleUsersEmailswithIndex(Dictionary<string, object> properties, Dictionary<string, object> parameters)
+        {
+            K2RolesSOProperties _properties = new K2RolesSOProperties(_K2ServerName, _PortNo);
+            DataTable result;
+            result = _properties.RoleUsersEmailswithIndex(properties, parameters);
+            return result;
+        }
+
+        private DataTable RoleUsersEmails(Dictionary<string, object> properties, Dictionary<string, object> parameters)
+        {
+           K2RolesSOProperties _properties = new K2RolesSOProperties(_K2ServerName, _PortNo);
+            DataTable result;
+            result = _properties.RoleUsersEmails(properties, parameters);
+            return result;
+        }
+
+        private DataTable IsinRole(Dictionary<string, object> properties, Dictionary<string, object> parameters)
+        {
+            K2RolesSOProperties _properties = new K2RolesSOProperties(_K2ServerName, _PortNo);
+            DataTable result;
+            result = _properties.IsinRole(properties, parameters);
+            return result;
+        }
+
+        private void ValidateConfigSection()
+        {
+            ServiceConfiguration config = base.Service.ServiceConfiguration;
+            _PortNo = Convert.ToInt32(config["Port"]);
+            _K2ServerName = config["K2ServerName"].ToString();
+
+            if (_PortNo == 0)
+            {
+                base.ServicePackage.IsSuccessful = false;
+                base.ServicePackage.ServiceMessages.Add(new ServiceMessage("Please provide SmartObject Port details ", MessageSeverity.Error));
+            }
+            if (string.IsNullOrEmpty(_K2ServerName))
+            {
+                base.ServicePackage.IsSuccessful = false;
+                base.ServicePackage.ServiceMessages.Add(new ServiceMessage("Please provide K2 ServerName to proceed further.", MessageSeverity.Error));
+            }
+        }
+
+    }
+}
---
+++ K2RolesSOProperties.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SourceCode.SmartObjects.Services.ServiceSDK.Types;
+using SourceCode.SmartObjects.Services.ServiceSDK.Objects;
+using SourceCode.Hosting.Client.BaseAPI;
+//using SourceCode.SmartObjects.Client;
+using System.Data;
+
+
+namespace K2ME.Field.Roles
+{
+    public class K2RolesSOProperties
+    {
+        #region private data members
+        
+        private string _K2ServerName;
+        private int _PortNo;
+        SourceCode.SmartObjects.Client.SmartObjectClientServer cServer;
+        
+        #endregion 
+
+        #region contructor(s)
+        
+        internal K2RolesSOProperties()
+        {
+        }
+
+        internal K2RolesSOProperties(string K2serverName, int portNo)
+        {
+            _K2ServerName = K2serverName;
+            _PortNo = portNo;
+        }
+        
+        #endregion
+
+        internal ServiceObject DescribeServiceObjects()
+        {
+            ServiceObject so = new ServiceObject("K2FieldRoles");
+            so.Type = "K2FieldRoles";
+            so.Active = true;
+            so.MetaData.DisplayName = "K2Field Roles";
+            so.MetaData.Description = "It gets you user information belongs to role with email address";
+            so.Methods = DescribeMethods();
+            so.Properties = DescribeProperties();
+            return so;
+        }
+
+        private SourceCode.SmartObjects.Services.ServiceSDK.Objects.Properties DescribeProperties()
+        {
+
+            Properties properties = new Properties();
+
+            properties.Add(new Property("Role_Name", "System.String", SoType.Text, new MetaData("RoleName", "RoleName")));
+            properties.Add(new Property("Name", "System.String", SoType.Text, new MetaData("Username", "Username")));
+            properties.Add(new Property("UserIndex", "System.Int32", SoType.Number, new MetaData("UserIndex", "UserIndex")));
+            properties.Add(new Property("EmailAddress", "System.String", SoType.Text, new MetaData("EmailAddress", "EmailAddress")));
+            properties.Add(new Property("IsinRole", "System.Boolean", SoType.YesNo, new MetaData("IsinRole", "IsinRole")));
+
+            return properties;
+        }
+
+        private SourceCode.SmartObjects.Services.ServiceSDK.Objects.Methods DescribeMethods()
+        {
+            SourceCode.SmartObjects.Services.ServiceSDK.Objects.Methods methods = new Methods();
+
+            //methods.Add(new Method("GetRoleUsersEmailAddress", MethodType.List, new MetaData("GetRoleUsersEmailAddress", "Returns Role Users Email Address."), GetRequiredProperties("GetRoleUserEmailAddress"), GetMethodParameters(), GetInputProperties("GetRoleUserEmailAddress"), GetReturnProperties("GetRoleUserEmailAddress")));
+
+            methods.Add(new Method("IsInRole", MethodType.List, new MetaData("IsInRole", "Return User presence in the K2 Role."), GetRequiredProperties("IsInRole"), GetMethodParameters(), GetInputProperties("IsInRole"), GetReturnProperties("IsInRole")));
+
+            methods.Add(new Method("GetRoleUsersEmailsonIndex", MethodType.List, new MetaData("GetRoleUsersEmailsonIndex", "Returns Role Users Email Address."), GetRequiredProperties("GetRoleUsersEmailsonIndex"), GetMethodParameters(), GetInputProperties("GetRoleUsersEmailsonIndex"), GetReturnProperties("GetRoleUsersEmailsonIndex")));
+
+            methods.Add(new Method("GetRoleUsersEmails", MethodType.List, new MetaData("GetRoleUsersEmails", "Returns Role Users Email Address."), GetRequiredProperties("GetRoleUsersEmails"), GetMethodParameters(), GetInputProperties("GetRoleUsersEmails"), GetReturnProperties("GetRoleUsersEmails")));
+
+            return methods;
+        }
+
+        private InputProperties GetInputProperties(string Method)
+        {
+            InputProperties properties = new InputProperties();
+            switch (Method)
+            {
+                case "IsInRole":
+                    #region properties
+                    properties.Add(new Property("Role_Name", "System.String", SoType.Text, new MetaData("Role_Name", "Role_Name")));
+                    properties.Add(new Property("IsinRole", "System.Boolean", SoType.YesNo, new MetaData("IsinRole", "IsinRole")));
+                    properties.Add(new Property("Name", "System.String", SoType.Text, new MetaData("Username", "Username")));
+
+                    break;
+                case "GetRoleUsersEmailAddress":
+
+                    properties.Add(new Property("Role_Name", "System.String", SoType.Text, new MetaData("Role_Name", "Role_Name")));
+
+                    break;
+                case "GetRoleUsersEmailsonIndex":
+                    properties.Add(new Property("Role_Name", "System.String", SoType.Text, new MetaData("Role_Name", "Role_Name")));
+                    properties.Add(new Property("UserIndex", "System.Int32", SoType.Number, new MetaData("UserIndex", "UserIndex")));
+                    break;
+
+                case "GetRoleUsersEmails":
+                    properties.Add(new Property("Role_Name", "System.String", SoType.Text, new MetaData("Role_Name", "Role_Name")));
+                    //properties.Add(new Property("UserIndex", "System.Int32", SoType.Number, new MetaData("UserIndex", "UserIndex")));
+                    break;
+
+                    #endregion
+            }
+            return properties;
+        }
+
+        private Validation GetRequiredProperties(string method)
+        {
+            RequiredProperties properties = new RequiredProperties();
+            Validation validation = null;
+            validation = new Validation();
+            switch (method)
+            {
+                case "IsInRole":
+                    #region properties
+                    properties.Add(new Property("Role_Name", "System.String", SoType.Text, new MetaData("Role_Name", "Role_Name")));
+                    properties.Add(new Property("Username", "System.String", SoType.Text, new MetaData("Username", "Username")));
+
+                    break;
+                case "GetRoleUsersEmailAddress":
+                    properties.Add(new Property("Role_Name", "System.String", SoType.Text, new MetaData("Role_Name", "Role_Name")));
+                    break;
+                case "GetRoleUsersEmailsonIndex":
+                    properties.Add(new Property("Role_Name", "System.String", SoType.Text, new MetaData("Role_Name", "Role_Name")));
+                    properties.Add(new Property("UserIndex", "System.Int32", SoType.Number, new MetaData("UserIndex", "UserIndex")));
+                    break;
+
+                case "GetRoleUsersEmails":
+                    properties.Add(new Property("Role_Name", "System.String", SoType.Text, new MetaData("Role_Name", "Role_Name")));
+                    //properties.Add(new Property("UserIndex", "System.Int32", SoType.Number, new MetaData("UserIndex", "UserIndex")));
+                    break;
+
+                    #endregion
+            }
+            validation.RequiredProperties = properties;
+            return validation;
+        }
+
+        private MethodParameters GetMethodParameters()
+        {
+            MethodParameters parameters = new MethodParameters();
+            
+            return parameters;
+        }
+
+        private ReturnProperties GetReturnProperties(string method)
+        {
+            ReturnProperties properties = new ReturnProperties();
+            switch (method)
+            {
+                case "IsInRole":
+                    properties.Add(new Property("IsinRole", "System.Boolean", SoType.YesNo, new MetaData("IsinRole", "IsinRole")));
+                    break;
+
+                case "GetRoleUsersEmailAddress":
+                    properties.Add(new Property("EmailAddress", "System.String", SoType.Text, new MetaData("EmailAddress", "EmailAddress")));
+                    break;
+
+                case "GetRoleUsersEmailsonIndex":
+                    properties.Add(new Property("EmailAddress", "System.String", SoType.Text, new MetaData("EmailAddress", "EmailAddress")));
+                    break;
+
+                case "GetRoleUsersEmails":
+                    properties.Add(new Property("EmailAddress", "System.String", SoType.Text, new MetaData("EmailAddress", "EmailAddress")));
+                    //properties.Add(new Property("UserIndex", "System.Int32", SoType.Number, new MetaData("UserIndex", "UserIndex")));
+                    break;
+            }
+            return properties;
+        }
+
+        public DataTable CallSmartObjectListMethod( Dictionary<string, object> parameters, Dictionary<string, object> properties)
+        {
+            string methodName = "Get_Role_Users";
+            string objectName = "UMUser";
+            Check_SmartObjectClientServer();
+            DataTable dt = null;
+            List<Dictionary<string, object>> returnCollection = new List<Dictionary<string, object>>();
+            if (!cServer.Connection.IsConnected) return new DataTable();
+
+            SourceCode.SmartObjects.Client.SmartObject smartObject = cServer.GetSmartObject(objectName);
+            smartObject.MethodToExecute = methodName;
+
+            //Set Parameter
+            foreach (SourceCode.SmartObjects.Client.SmartParameter para in smartObject.ListMethods[methodName].Parameters)
+            {
+                if (properties.ContainsKey(para.Name)) SetPropertyValue(para, properties[para.Name]);
+            }
+
+            //Set propery
+            foreach (SourceCode.SmartObjects.Client.SmartProperty prop in smartObject.ListMethods[methodName].InputProperties)
+            {
+                //See if this key exists in the data properteries
+                if (properties.ContainsKey(prop.Name)) SetPropertyValue(prop, properties[prop.Name]);
+            }
+            dt = cServer.ExecuteListDataTable(smartObject);
+            cServer.Connection.Close();
+            smartObject = null;
+            return dt;
+        }
+
+        private void SetPropertyValue(SourceCode.SmartObjects.Client.SmartProperty smartProperty, object value)
+        {
+            if (value == null)
+            {
+                smartProperty.ValueBehaviour = SourceCode.SmartObjects.Client.ValueBehaviour.Unchanged;
+                smartProperty.Value = null;
+            }
+            else if (value == DBNull.Value)
+            {
+                smartProperty.ValueBehaviour = SourceCode.SmartObjects.Client.ValueBehaviour.Clear;
+                smartProperty.Value = null;
+            }
+            else if (value.ToString() == string.Empty)
+            {
+                smartProperty.ValueBehaviour = SourceCode.SmartObjects.Client.ValueBehaviour.Empty;
+                smartProperty.Value = string.Empty;
+            }
+            else
+            {
+                smartProperty.ValueBehaviour = SourceCode.SmartObjects.Client.ValueBehaviour.None;
+                smartProperty.Value = value.ToString();
+            }
+        }
+        
+        private void Check_SmartObjectClientServer()
+        {
+            string connStr = string.Format("Integrated=True;IsPrimaryLogin=True;Authenticate=True;EncryptedPassword=False;Host={0};Port={1}", _K2ServerName, _PortNo);
+            try
+            {
+                if (cServer == null)
+                {
+                    cServer = new SourceCode.SmartObjects.Client.SmartObjectClientServer();
+                    cServer.CreateConnection();
+                    cServer.Connection.Open(connStr);
+                }
+            }
+            catch
+            {
+                throw new Exception("Couldn't create connection to server");
+            }
+        }
+
+        public DataTable IsinRole(Dictionary<string, object> properties, Dictionary<string, object> parameters)
+        {
+            DataTable ret = new DataTable();
+            DataRow dr1;
+            bool set = false;
+            DataTable results = CallSmartObjectListMethod(parameters, properties);
+            foreach(DataRow dr in results.Rows)
+            {
+                if (dr["Name"].ToString().ToLower().Equals(properties["Name"].ToString().ToLower()))
+                    set = true;
+            }
+            dr1 = ret.NewRow();
+            ret.Columns.Add("IsinRole");
+            dr1["IsinRole"] = set;
+            ret.Rows.Add(dr1);
+            return ret ;
+        }
+
+        public DataTable RoleUsersEmails(Dictionary<string, object> properties, Dictionary<string, object> parameters)
+        {
+            DataTable ret = new DataTable();
+            DataRow dr1;
+            Check_SmartObjectClientServer();
+            string RoleEmailAddress = string.Empty;
+
+            DataTable results = CallSmartObjectListMethod(parameters, properties);
+            foreach (DataRow dr in results.Rows)
+            {
+               RoleEmailAddress = RoleEmailAddress + dr["Email"].ToString() + ";";
+            }
+            dr1 = ret.NewRow();
+            ret.Columns.Add("EmailAddress");
+            dr1["EmailAddress"] = RoleEmailAddress;
+            ret.Rows.Add(dr1);
+            return ret;
+        }
+
+        public DataTable RoleUsersEmailswithIndex(Dictionary<string, object> properties, Dictionary<string, object> parameters)
+        {
+            DataTable ret = new DataTable();
+            DataRow dr1;
+            Check_SmartObjectClientServer();
+            string RoleEmailAddress = string.Empty;
+            
+            int UserIndex = Convert.ToInt16(properties["UserIndex"]);
+            DataTable results = CallSmartObjectListMethod(parameters, properties);
+            int rowsCount = results.Rows.Count;
+            for (int i = 0; i < rowsCount; i++ )
+            {
+                if(i != UserIndex)
+                { RoleEmailAddress = results.Rows[i]["Email"].ToString() + ";"; }
+            }
+            dr1 = ret.NewRow();
+            ret.Columns.Add("EmailAddress");
+            dr1["EmailAddress"] = RoleEmailAddress;
+            ret.Rows.Add(dr1);
+            return ret;
+        }
+
+        public DataTable RoleUsersEmailsExceptIndex(Dictionary<string, object> properties, Dictionary<string, object> parameters)
+        {
+            DataTable ret = new DataTable();
+            DataRow dr1;
+            Check_SmartObjectClientServer();
+            string RoleEmailAddress = string.Empty;
+
+            int UserIndex = Convert.ToInt16(properties["UserIndex"]);
+            DataTable results = CallSmartObjectListMethod(parameters, properties);
+            int rowsCount = results.Rows.Count;
+            for (int i = 0; i < rowsCount; i++)
+            {
+                if (i != UserIndex)
+                { RoleEmailAddress = results.Rows[i]["Email"].ToString() + ";"; }
+            }
+            dr1 = ret.NewRow();
+            ret.Columns.Add("EmailAddress");
+            dr1["EmailAddress"] = RoleEmailAddress;
+            ret.Rows.Add(dr1);
+            return ret;
+        }
+    }
+}
